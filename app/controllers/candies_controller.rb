df1095a666b45412749e31b326e579b642d165e4
Alberto Mayanga <amayanga90@yahoo.com>
2017-11-10 23:22:21 +0000

    implemented adding candy to bag feature

--- app/controllers/candies_controller.rb
+++ app/controllers/candies_controller.rb
@@ -1,10 +1,24 @@
 class CandiesController < ApplicationController
 
-  get 'candy/new' do
+  get '/candy/:id/new' do
     if logged_in?
-      erb :"candies/new"
+      @bag = Bag.find_by_id(params[:id])
+      erb :"candies/create_candy"
     else
       redirect to '/login'
+    end
+  end
+
+  post '/candy/:bagid' do
+    if params[:name] == nil
+      @bag = Bag.find_by_id(params[:bagid])
+      redirect to "/candy/#{@bag.id}/new"
+    else
+      @user = User.find_by_id(session[:user_id])
+      @bag = Bag.find_by_id(params[:bagid])
+      @candy = Candy.create(name: params[:name],rating: params[:rating])
+      @bag.candies << @candy
+      redirect to "/bags/#{@user.username}/#{@bag.id}"
     end
   end
 
